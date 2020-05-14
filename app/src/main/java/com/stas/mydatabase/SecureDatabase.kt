3ef1d28cefc011c87aa8e055a4257f4e1d43f15b
stas <webnetinterent@gmail.com>
2020-05-14 15:48:06 +0300

    initial

--- app/src/main/java/com/stas/mydatabase/SecureDatabase.kt
+++ app/src/main/java/com/stas/mydatabase/SecureDatabase.kt
@@ -1,77 +1,77 @@
-package com.stas.mydatabase
-
-import android.app.ProgressDialog.show
-import android.content.Context
-import android.widget.Toast
-import androidx.room.*
-import net.sqlcipher.database.SQLiteDatabase
-import net.sqlcipher.database.SupportFactory
-
-
-object SecureDatabase {
-
-    //    val db = Room.databaseBuilder(
-//        applicationContext,
-//        AppDatabase::class.java, "database-name"
-//    ).build()
+//package com.stas.mydatabase
 //
-    fun initDb(context: Context) {
-        val passphrase: ByteArray = SQLiteDatabase.getBytes(charArrayOf('s', 't'))
-        val factory = SupportFactory(passphrase)
-        val room: AppDatabase = Room.databaseBuilder(
-            context,
-            AppDatabase::class.java, "AppDatabase"
-        )
-            .openHelperFactory(factory).allowMainThreadQueries()
-            .build()
-//        room.userDao().insertAll(
-//            User(1, "", "s", 4),
-//            User(2, "asd", "sd", 3)
+//import android.app.ProgressDialog.show
+//import android.content.Context
+//import android.widget.Toast
+//import androidx.room.*
+//import net.sqlcipher.database.SQLiteDatabase
+//import net.sqlcipher.database.SupportFactory
+//
+//
+//object SecureDatabase {
+//
+//    //    val db = Room.databaseBuilder(
+////        applicationContext,
+////        AppDatabase::class.java, "database-name"
+////    ).build()
+////
+//    fun initDb(context: Context) {
+//        val passphrase: ByteArray = SQLiteDatabase.getBytes(charArrayOf('s', 't'))
+//        val factory = SupportFactory(passphrase)
+//        val room: AppDatabase = Room.databaseBuilder(
+//            context,
+//            AppDatabase::class.java, "AppDatabase"
 //        )
-
-        Toast.makeText(context, "sum: ${room.userDao().getUser()}", Toast.LENGTH_SHORT).show()
-
-    }
-
-    @Database(entities = [User::class], version = 1)
-    abstract class AppDatabase : RoomDatabase() {
-        abstract fun userDao(): UserDao
-    }
-
-    @Entity
-    data class User(
-        @PrimaryKey val uid: Int,
-        @ColumnInfo(name = "first_name") val firstName: String?,
-        @ColumnInfo(name = "last_name") val lastName: String?,
-        @ColumnInfo(name = "number") val number: Int
-    )
-
-
-    @Dao
-    interface UserDao {
-        @Query("SELECT * FROM user")
-        fun getAll(): List<User>
-
-        @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-        fun loadAllByIds(userIds: IntArray): List<User>
-
-        @Query(
-            "SELECT * FROM user WHERE first_name LIKE :first AND " +
-                    "last_name LIKE :last LIMIT 1"
-        )
-        fun findByName(first: String, last: String): User
-
-        @Query("SELECT SUM(number) FROM user")
-        fun sumUsers(): Int
-
-        @Query("SELECT last_name FROM user where number = 3")
-        fun getUser(): String
-
-        @Insert
-        fun insertAll(vararg users: User)
-
-        @Delete
-        fun delete(user: User)
-    }
-
-}
+//            .openHelperFactory(factory).allowMainThreadQueries()
+//            .build()
+////        room.userDao().insertAll(
+////            User(1, "", "s", 4),
+////            User(2, "asd", "sd", 3)
+////        )
+//
+//        Toast.makeText(context, "sum: ${room.userDao().getUser()}", Toast.LENGTH_SHORT).show()
+//
+//    }
+//
+//    @Database(entities = [User::class], version = 1)
+//    abstract class AppDatabase : RoomDatabase() {
+//        abstract fun userDao(): UserDao
+//    }
+//
+//    @Entity
+//    data class User(
+//        @PrimaryKey val uid: Int,
+//        @ColumnInfo(name = "first_name") val firstName: String?,
+//        @ColumnInfo(name = "last_name") val lastName: String?,
+//        @ColumnInfo(name = "number") val number: Int
+//    )
+//
+//
+//    @Dao
+//    interface UserDao {
+//        @Query("SELECT * FROM user")
+//        fun getAll(): List<User>
+//
+//        @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+//        fun loadAllByIds(userIds: IntArray): List<User>
+//
+//        @Query(
+//            "SELECT * FROM user WHERE first_name LIKE :first AND " +
+//                    "last_name LIKE :last LIMIT 1"
+//        )
+//        fun findByName(first: String, last: String): User
+//
+//        @Query("SELECT SUM(number) FROM user")
+//        fun sumUsers(): Int
+//
+//        @Query("SELECT last_name FROM user where number = 3")
+//        fun getUser(): String
+//
+//        @Insert
+//        fun insertAll(vararg users: User)
+//
+//        @Delete
+//        fun delete(user: User)
+//    }
+//
+//}
--- app/src/main/java/com/stas/mydatabase/activities/MainActivity.kt
+++ app/src/main/java/com/stas/mydatabase/activities/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.stas.mydatabase
+package com.stas.mydatabase.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stas.mydatabase.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        SecureDatabase.initDb(this)
+//        SecureDatabase.initDb(this)
     }
 }
