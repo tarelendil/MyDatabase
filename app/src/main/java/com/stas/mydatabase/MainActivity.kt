96c755a98eda95b8a046efbe54faaa10123feeca
stas <webnetinterent@gmail.com>
2020-05-14 15:40:14 +0300

    initial

---
+++ app/src/main/java/com/stas/mydatabase/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.stas.mydatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        SecureDatabase.initDb(this)
+    }
+}
