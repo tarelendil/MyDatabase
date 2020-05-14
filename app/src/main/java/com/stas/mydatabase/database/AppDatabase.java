7d1471dbb15ea2202115cd39fa48ea27bbb61f19
stas <webnetinterent@gmail.com>
2020-05-14 16:09:28 +0300

    setup

---
+++ app/src/main/java/com/stas/mydatabase/database/AppDatabase.java
@@ -0,0 +1,12 @@
+package com.stas.mydatabase.database;
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+}
+
+
---
+++ app/src/main/java/com/stas/mydatabase/database/DatabaseManager.java
@@ -0,0 +1,23 @@
+package com.stas.mydatabase.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.Room;
+
+import com.stas.mydatabase.application.App;
+
+public class DatabaseManager {
+    private static DatabaseManager instance = new DatabaseManager();
+
+    @NonNull
+    private AppDatabase appDatabase;
+
+    public static DatabaseManager getInstance() {
+        return instance;
+    }
+
+
+    private DatabaseManager() {
+        appDatabase = Room.databaseBuilder(App.getAppContext(),
+                AppDatabase.class, "AppDatabase").build();
+    }
+}
